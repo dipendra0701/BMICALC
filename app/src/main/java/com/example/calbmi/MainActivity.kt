c85d4a393398cb530826abbe427d8a6b33234837
dipendra pandey <pandeydipendra678@gmail.com>
2023-03-07 12:25:11 +0530

    BMI cmt 3rd

--- app/src/main/java/com/example/calbmi/MainActivity.kt
+++ app/src/main/java/com/example/calbmi/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.calbmi
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Toast
@@ -7,40 +8,61 @@ import com.example.calbmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
      private lateinit var binding: ActivityMainBinding
+     private var isClear : Boolean = false
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCal.setOnClickListener {
-            if (binding.edtWeight.text.toString().isNotEmpty() && binding.edtHeight.text.toString().isNotEmpty()) {
-                val wt = (binding.edtWeight.text.toString()).toDouble()
-                val ht = (binding.edtHeight.text.toString()).toDouble()
-                val htnew = (ht / 100).toDouble()
-                val bmi = wt / (htnew * htnew)
-                binding.txtMsg.text = R.string.strBMI.toString()
-                binding.txtResult.text = " Your BMI is ${bmi.toInt()}"
-                if (bmi > 25) {
-                    Toast.makeText(this@MainActivity, R.string.strOverweight, Toast.LENGTH_SHORT)
-                        .show()
-                    binding.txtMsg.text = resources.getString(R.string.strOverweight)
-                } else if (bmi < 18) {
-                    Toast.makeText(this@MainActivity, R.string.strUnderweight, Toast.LENGTH_SHORT)
-                        .show()
-                    binding.txtMsg.text = resources.getString(R.string.strUnderweight)
-                } else  if(bmi>18&&bmi<25) {
-                    Toast.makeText(this@MainActivity, R.string.strHealthy, Toast.LENGTH_SHORT)
-                        .show()
-                    binding.txtMsg.text = resources.getString(R.string.strHealthy)
+        binding.btnCal.setOnClickListener{
+            if (isClear) {
+                isClear = false
+                binding.btnCal.text = "Calculate"
+            } else {
+                if (binding.edtWeight.text.toString()
+                        .isNotEmpty() && binding.edtHeight.text.toString().isNotEmpty()
+                ) {
+                    if (!isClear){
+                        isClear = true
+                        binding.btnCal.text = "Clear"
+                     binding.btnCal.setOnClickListener()
+
+                    }
+                    val wt = (binding.edtWeight.text.toString()).toDouble()
+                    val ht = (binding.edtHeight.text.toString()).toDouble()
+                    val htnew = (ht / 100).toDouble()
+                    val bmi = wt / (htnew * htnew)
+                    binding.txtMsg.text = R.string.strBMI.toString()
+                    binding.txtResult.text = " Your BMI is ${bmi.toInt()}"
+                    if (bmi > 25) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            R.string.strOverweight,
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        binding.txtMsg.text = resources.getString(R.string.strOverweight)
+                    } else if (bmi < 18) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            R.string.strUnderweight,
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        binding.txtMsg.text = resources.getString(R.string.strUnderweight)
+                    } else if (bmi > 18 && bmi < 25) {
+                        Toast.makeText(this@MainActivity, R.string.strHealthy, Toast.LENGTH_SHORT)
+                            .show()
+                        binding.txtMsg.text = resources.getString(R.string.strHealthy)
+                    }
+
+
+                } else {
+                    Toast.makeText(this@MainActivity, R.string.strinvalid, Toast.LENGTH_LONG).show()
+                    binding.txtMsg.text = resources.getString(R.string.strinvalid)
+
                 }
-
-
-            }
-            else
-            {
-               Toast.makeText(this@MainActivity,R.string.strinvalid, Toast.LENGTH_LONG).show()
-                binding.txtMsg.text = resources.getString(R.string.strinvalid)
-
             }
         }
 
